38dedbe6fc891fb83da092d9c6879abdf6d6f15a
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-04 16:24:18 -0300

    Scripts para criar o consenso da filogenia

---
+++ consenso_filogenia.R
@@ -0,0 +1,53 @@
+# Pacotes ----
+
+library(ape)
+
+library(tidyverse)
+
+library(phytools)
+
+# Dados ----
+
+## Árvore filogenética ----
+
+### Importando ----
+
+tree <- ape::read.nexus("output3.nex")
+
+### Visualizando ----
+
+tree
+
+#tree |> ape::plot.multiPhylo(type = "fan")
+
+# Consenso ----
+
+## Calculando o consenso ----
+
+tree_cons <- tree |>
+  phytools::consensus.edges(if.absent = "ignore",
+                            collapse = FALSE)
+
+## Visualizando ----
+
+tree_cons
+
+tree_cons |> ape::plot.phylo(type = "fan",
+                             show.tip.label = TRUE,
+                             edge.color = "blue",
+                             edge.width = 1.5,
+                             tip.color = "black",
+                             cex = 0.45,
+                             label.offset = 0.001)
+
+# Histograma dos comprimentos dos ramos ----
+
+tree_cons$edge.length |>
+  data.frame() |>
+  dplyr::rename("Comprimento dos ramos" = 1) |>
+  ggplot(aes(`Comprimento dos ramos`)) +
+  geom_histogram(color = 'black', binwidth = 5)
+
+# Exportando ----
+
+tree_cons |> ape::write.tree("tree_cons.phy")
