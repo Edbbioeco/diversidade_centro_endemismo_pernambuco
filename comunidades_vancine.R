8b05adda58874919c4419e5093cbb187e1d9b75f
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-16 18:54:27 -0300

    Scripts para montar as copmunidades através dos dados do datapaper de Vancine et al. 2018

--- comunidades_vancine.R
+++ comunidades_vancine.R
@@ -19,3 +19,17 @@ oc_vancine <- readr::read_csv("C:/Users/LENOVO/OneDrive/Documentos/curso_diversi
 oc_vancine
 
 oc_vancine |> dplyr::glimpse()
+
+## Grade ----
+
+### Importando ----
+
+grade_cep <- sf::st_read("cep_grade.shp")
+
+### Visualizando ----
+
+grade_cep
+
+grade_cep |>
+  ggplot() +
+  geom_sf(color = "black", fill = "green4")
