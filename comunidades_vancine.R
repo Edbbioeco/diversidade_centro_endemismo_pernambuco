796916d1665a18cb56955b63bd3010db2d0e85f8
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-16 18:56:05 -0300

    Scripts para montar as copmunidades através dos dados do datapaper de Vancine et al. 2018

--- comunidades_vancine.R
+++ comunidades_vancine.R
@@ -8,6 +8,12 @@ library(writexl)
 
 # Dados ----
 
+## Identificação das espécies ----
+
+### Importando ----
+
+### visualizando ----
+
 ## Ocorrências ----
 
 ### Importando ----
@@ -19,6 +25,36 @@ oc_vancine <- readr::read_csv("C:/Users/LENOVO/OneDrive/Documentos/curso_diversi
 oc_vancine
 
 oc_vancine |> dplyr::glimpse()
+
+### Tratando ----
+
+oc_gbif_trat <- oc_vancine |>
+  dplyr::select(species, stateProvince, decimalLatitude:decimalLongitude) |>
+  dplyr::rename("Latitude" = decimalLatitude,
+                "Longitude" = decimalLongitude) |>
+  dplyr::mutate(Longitude = Longitude |>
+                  stringr::str_replace("^(-?\\d{2})(\\d+)$", "\\1.\\2") |>
+                  as.numeric(),
+                Latitude = case_when(stringr::str_detect(
+                  as.character(Latitude),
+                  "^(-?[1-2])") ~ str_replace(as.character(Latitude),
+                                              "^(-?\\d{2})(\\d+)$", "\\1.\\2"),
+                  stringr::str_detect(
+                    as.character(Latitude),
+                    "^(-?[3-9])") ~ stringr::str_replace(as.character(Latitude),
+                                                         "^(-?\\d{1})(\\d+)$", "\\1.\\2"),
+                  TRUE ~ as.character(Latitude)) |>
+                  as.numeric()) |>
+  dplyr::filter(!is.na(species) &
+                  !is.na(Latitude) &
+                  !is.na(Longitude) &
+                  !species |> stringr::str_detect(" sp| cf| af") &
+                  species |> stringr::word(2) != "NA") |>
+  dplyr::distinct(species, Longitude, Latitude, .keep_all = TRUE)
+
+oc_gbif_trat |> dplyr::glimpse()
+
+oc_gbif_trat
 
 ## Grade ----
 
