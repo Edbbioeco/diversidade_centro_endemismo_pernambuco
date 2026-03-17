80214bdd68cd9fb6f1d95448fe94f76af444c0ae
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-17 09:16:14 -0300

    Scripts para montar as comunidades do iNatualist usando o pacote rinat

--- comunidades_inaturalist.R
+++ comunidades_inaturalist.R
@@ -1,2 +1,26 @@
 # Pacotes ----
 
+library(sf)
+
+library(tidyverse)
+
+library(rinat)
+
+library(writexl)
+
+# Dados ----
+
+## Grade ----
+
+### Importando ----
+
+grade <- sf::st_read("cep_grade.shp")
+
+### Visualizando ----
+
+grade
+
+ggplot() +
+  geom_sf(data = grade, color = "forestgreen", fill = "transparent")
+
+## Registros de ocorrência do iNaturalist ----
