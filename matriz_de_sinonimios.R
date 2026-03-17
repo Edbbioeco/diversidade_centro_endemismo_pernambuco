0ead7e738553c09623fa30effce5b29cdc415357
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-17 14:47:14 -0300

    Scripts para criar a matriz base de sinonimios

--- matriz_de_sinonimios.R
+++ matriz_de_sinonimios.R
@@ -18,6 +18,35 @@ registros
 
 registros |> dplyr::glimpse()
 
+# Matriz ----
+
+## Montando a matriz ----
+
+matriz_sin <- registros |>
+  dplyr::select(2:3) |>
+  dplyr::mutate(synonym = NA,
+                `vertlif present` = NA,
+                `present as` = NA,
+                replaced = NA,
+                `replaced species` = NA)
+
+matriz_sin
+
+## Exportando ----
+
+mat
+
+
+
+
+
+
+
+
+
+
+
+
 
 matriz_trat |>
   dplyr::select(7:103) |>
